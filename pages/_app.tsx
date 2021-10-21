45872fc2d808471bfb49989656d21adf4848b3c8
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-10-21 22:10:21 +0900

    reset relay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
       <Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import React, { Suspense } from "react";
-import { loadQuery, RelayEnvironmentProvider } from "react-relay/hooks";
-
-import { App, RepositoryNameQuery } from "../components/app";
-import { RelayEnvironment } from "../lib/relay-environment";
+import { Container, Box } from "@chakra-ui/react";
 
 import type { NextPage } from "next";
 
-const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery, {
-  /* query variables */
-});
-
 const Home: NextPage = () => {
   return (
-    <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <Suspense fallback={"Loading..."}>
-        <App preloadedQuery={preloadedQuery} />
-      </Suspense>
-    </RelayEnvironmentProvider>
+    <Container maxW="container.xl">
+      <Box p={4}>Hello, World</Box>
+    </Container>
   );
 };
 
