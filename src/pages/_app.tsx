01563180d3ce5b2d6db67bb54dc6e75cfd733d8d
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-10-27 22:50:58 +0900

    remove relay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
-import { RelayEnvironmentProvider } from "react-relay";
-
-import initEnvironment from "lib/relay-environment";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const { environment } = initEnvironment();
   return (
     <ChakraProvider>
       <Head>
         <title>github releases</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <RelayEnvironmentProvider environment={environment}>
-        <Component {...pageProps} />
-      </RelayEnvironmentProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,13 @@
 import { Container, Box } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { usePreloadedQuery, useQueryLoader } from "react-relay";
-import { graphql } from "relay-runtime";
 
 import type { NextPage } from "next";
 
-const RepositoryNameQuery = graphql`
-  query pagesRepositoryNameQuery {
-    repository(owner: "facebook", name: "relay") {
-      name
-    }
-  }
-`;
-
 const Home: NextPage = () => {
-  const [queryReference, loadQuery] = useQueryLoader(RepositoryNameQuery);
-  useEffect(() => {
-    if (!queryReference) {
-      loadQuery({});
-    }
-  }, [queryReference, loadQuery]);
-
   return (
     <Container maxW="container.xl">
-      {queryReference ? <App queryRef={queryReference} /> : null}
+      <Box p={4}>Hello, world</Box>
     </Container>
   );
-};
-
-const App = ({ queryRef }) => {
-  console.log(queryRef);
-  const data = usePreloadedQuery(RepositoryNameQuery, queryRef);
-  console.log(data);
-  return <Box p={4}>Hello, world</Box>;
 };
 
 export default Home;
