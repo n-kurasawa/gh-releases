e4aa7165e813f9ea401138a6f423884d2acca6a3
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-11-06 12:57:32 +0900

    graphql request

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const token = process.env.NEXT_PUBLIC_REACT_APP_GITHUB_AUTH_TOKEN;
+
 export const client = new ApolloClient({
-  uri: "https://countries.trevorblades.com",
+  uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
+  headers: { authorization: `Bearer ${token}` },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
+import { useQuery, gql } from "@apollo/client";
 import { Container, Box } from "@chakra-ui/react";
 
 import type { NextPage } from "next";
 
+const Query = gql`
+  query {
+    viewer {
+      login
+    }
+  }
+`;
+
 const Home: NextPage = () => {
+  const { data, loading, error } = useQuery(Query);
+  if (loading) {
+    return (
+      <Container maxW="container.xl">
+        <Box p={4}>
+          <div>loading...</div>
+        </Box>
+      </Container>
+    );
+  }
+
+  if (error) {
+    console.error(error);
+    return (
+      <Container maxW="container.xl">
+        <Box p={4}>
+          <div>error</div>
+        </Box>
+      </Container>
+    );
+  }
+
   return (
     <Container maxW="container.xl">
-      <Box p={4}>Hello, world</Box>
+      <Box p={4}>{data.viewer.login}</Box>
     </Container>
   );
 };
