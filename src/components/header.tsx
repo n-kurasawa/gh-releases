4c187ca690e973c5bfdccf8e323ae140669e2f64
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-11-20 00:33:22 +0900

    rm

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -254,21 +254,6 @@ const NAV_ITEMS: Array<NavItem> = [
     ],
   },
   {
-    label: "Find Work",
-    children: [
-      {
-        label: "Job Board",
-        subLabel: "Find your dream design job",
-        href: "#",
-      },
-      {
-        label: "Freelance Projects",
-        subLabel: "An exclusive list for contract work",
-        href: "#",
-      },
-    ],
-  },
-  {
     label: "Learn Design",
     href: "#",
   },
